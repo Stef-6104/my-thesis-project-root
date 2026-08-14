c7b524122643412a0f7d56ccd33913d70b29ba1f
grantlu-24-boop <grant.lu-24@cpu.edu.ph>
2026-08-14 06:57:46 -0400

    attempted fix on most kotlin and flutter errors

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:my_thesis_project/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+    // Build our app and trigger a frame.f
     await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
